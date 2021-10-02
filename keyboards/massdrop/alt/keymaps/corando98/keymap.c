7b8cd9d9afe78582e19dc0ef5dae7266a3bcd135
Diego Garcia <diegocorando@gmail.com>
2021-10-01 21:49:50 -0500

    Updated personal keymap

--- keyboards/massdrop/alt/keymaps/corando98/keymap.c
+++ keyboards/massdrop/alt/keymaps/corando98/keymap.c
@@ -11,7 +11,7 @@ enum alt_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_65_ansi_blocker(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, LGUI(KC_BSPC), KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, LCA(KC_NO), LT(2,KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, LCAG_T(KC_RGHT), KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, LCAG_T(KC_LEFT), KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_RGUI, MO(1), KC_LEFT, KC_DOWN, KC_RGHT),
+    [0] = LAYOUT_65_ansi_blocker(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, LGUI(KC_BSPC), KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, LCA(KC_NO), LT(2,KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, LCAG(KC_RGHT), KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, LCAG(KC_LEFT), KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_RGUI, MO(1), KC_LEFT, KC_DOWN, KC_RGHT),
 	[1] = LAYOUT_65_ansi_blocker(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_MUTE, KC_TRNS, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, KC_TRNS, KC_TRNS, KC_TRNS, DBG_TOG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_END, KC_TRNS, RGB_RMOD, RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, KC_TRNS, VLK_TOG, DBG_KBD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU, KC_TRNS, RGB_TOG, KC_TRNS, KC_TRNS, KC_TRNS, MD_BOOT, NK_TOGG, DBG_MTRX, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_VOLD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END),
 	[2] = LAYOUT_65_ansi_blocker(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
     /*
@@ -36,80 +36,74 @@ bool Jelocikey_toggle = true;
 static uint16_t falloff_timer; //Custom timer to check if velocikey can decrease speed
 
 
-int16_t currentWPM = 0;
+int16_t currentWPM = 1;
 bool consecutive_key = false;  //Flag to know if velocikey should start decreasing after a key has not been pressed for Xms
+bool    KeyDown         = false;
+bool    process_record_user(uint16_t keycode, keyrecord_t *record) {
+    static uint32_t key_timer;
+    // idle_timer = timer_read();
+    falloff_timer = timer_read();
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-
-static uint32_t key_timer;
-//idle_timer = timer_read();
-falloff_timer = timer_read();
-
-
-switch (keycode) {
-    case U_T_AUTO:
-        if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
-            TOGGLE_FLAG_AND_PRINT(usb_extra_manual, "USB extra port manual mode");
-        }
-        return false;
-    case U_T_AGCR:
-        if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
-            TOGGLE_FLAG_AND_PRINT(usb_gcr_auto, "USB GCR auto mode");
-        }
-        return false;
-    case DBG_TOG:
-        if (record->event.pressed) {
-            TOGGLE_FLAG_AND_PRINT(debug_enable, "Debug mode");
-        }
-        return false;
-    case DBG_MTRX:
-        if (record->event.pressed) {
-            TOGGLE_FLAG_AND_PRINT(debug_matrix, "Debug matrix");
-        }
-        return false;
-    case DBG_KBD:
-        if (record->event.pressed) {
-            TOGGLE_FLAG_AND_PRINT(debug_keyboard, "Debug keyboard");
-        }
-        return false;
-    case DBG_MOU:
-        if (record->event.pressed) {
-            TOGGLE_FLAG_AND_PRINT(debug_mouse, "Debug mouse");
-        }
-        return false;
-    case MD_BOOT:
-        if (record->event.pressed) {
-            key_timer = timer_read32();
-        } else {
-            if (timer_elapsed32(key_timer) >= 500) {
-                reset_keyboard();
+    switch (keycode) {
+        case U_T_AUTO:
+            if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
+                TOGGLE_FLAG_AND_PRINT(usb_extra_manual, "USB extra port manual mode");
             }
-        }
-        return false;
-    case RGB_TOG:
-        if (record->event.pressed) {
-            switch (rgb_matrix_get_flags()) {
-                case LED_FLAG_ALL: {
-                    rgb_matrix_set_flags(LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER | LED_FLAG_INDICATOR);
-                    rgb_matrix_set_color_all(0, 0, 0);
-                  }
-                  break;
-                case (LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER | LED_FLAG_INDICATOR): {
-                    rgb_matrix_set_flags(LED_FLAG_UNDERGLOW);
-                    rgb_matrix_set_color_all(0, 0, 0);
-                  }
-                  break;
-                case LED_FLAG_UNDERGLOW: {
-                    rgb_matrix_set_flags(LED_FLAG_NONE);
-                    rgb_matrix_disable_noeeprom();
-                  }
-                  break;
-                default: {
-                    rgb_matrix_set_flags(LED_FLAG_ALL);
-                    rgb_matrix_enable_noeeprom();
-                  }
-                  break;
-              }
+            return false;
+        case U_T_AGCR:
+            if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
+                TOGGLE_FLAG_AND_PRINT(usb_gcr_auto, "USB GCR auto mode");
+            }
+            return false;
+        case DBG_TOG:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_enable, "Debug mode");
+            }
+            return false;
+        case DBG_MTRX:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_matrix, "Debug matrix");
+            }
+            return false;
+        case DBG_KBD:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_keyboard, "Debug keyboard");
+            }
+            return false;
+        case DBG_MOU:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_mouse, "Debug mouse");
+            }
+            return false;
+        case MD_BOOT:
+            if (record->event.pressed) {
+                key_timer = timer_read32();
+            } else {
+                if (timer_elapsed32(key_timer) >= 500) {
+                    reset_keyboard();
+                }
+            }
+            return false;
+        case RGB_TOG:
+            if (record->event.pressed) {
+                switch (rgb_matrix_get_flags()) {
+                    case LED_FLAG_ALL: {
+                        rgb_matrix_set_flags(LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER | LED_FLAG_INDICATOR);
+                        rgb_matrix_set_color_all(0, 0, 0);
+                    } break;
+                    case (LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER | LED_FLAG_INDICATOR): {
+                        rgb_matrix_set_flags(LED_FLAG_UNDERGLOW);
+                        rgb_matrix_set_color_all(0, 0, 0);
+                    } break;
+                    case LED_FLAG_UNDERGLOW: {
+                        rgb_matrix_set_flags(LED_FLAG_NONE);
+                        rgb_matrix_disable_noeeprom();
+                    } break;
+                    default: {
+                        rgb_matrix_set_flags(LED_FLAG_ALL);
+                        rgb_matrix_enable_noeeprom();
+                    } break;
+                }
             }
             return false;
         case VLK_TOG:
@@ -129,22 +123,30 @@ switch (keycode) {
             }
 
             if (Jelocikey_toggle) {
-                currentWPM = currentWPM + 3;
+                if(KeyDown){
+                    printf("KeyDown -> False\n");
 
-                if (currentWPM > 254) {
-                    currentWPM = 254;
+                    KeyDown = false;
+                } else { //Only increase speed on keydown status
+                    printf("KeyDown -> True\n");
+                    KeyDown = true;
+
+                    currentWPM = currentWPM + 5;
+                    printf("Increasing speed by %d\n", 5 + currentWPM * (255 / currentWPM));
+                    //currentWPM = currentWPM + 2 * currentWPM;
+                    if (currentWPM > 255) { currentWPM = 255; }
+                    printf("Increasing speed -> %d\n", currentWPM);
+
+
+                    rgb_matrix_set_speed_noeeprom(currentWPM);
+                    consecutive_key = true;
                 }
-                printf("Increasing speed -> %d\n", currentWPM);
 
-
-                rgb_matrix_set_speed_noeeprom(currentWPM);
-                //rgb_matrix_increase_speed();
-                //rgb_matrix_set_color(13, 100, 255, 255);
             }
 
-            consecutive_key = true;
 
-            return true; //Process all other keycodes normally
+
+            return true; //Process all other keycodes normallysadasdsds
     }
 }
 
@@ -173,11 +175,11 @@ void matrix_scan_user(void) {
 
         //consecutive_key = false;
         } else {
-            if (timer_elapsed(falloff_timer) > (1000 * 255 / currentWPM) && deccelerating) {  // i.e Two keys are not struck within 1 seconds; not consecutive
+            if (timer_elapsed(falloff_timer) > (1020 * 255 / currentWPM) && deccelerating) {  // i.e Two keys are not struck within 1 seconds; not consecutive
                 falloff_timer      = 0;
                 timer_clear();
 
-                currentWPM -= 12;
+                currentWPM -= 40;
                 if (currentWPM < 1) {
                     currentWPM = 1;
                     deccelerating = false;
@@ -215,42 +217,54 @@ layer_state_t layer_state_setuser(layer_state_t state) {
 void rgb_matrix_indicators_user(void) {
     switch(get_highest_layer(layer_state)) {
         case 0:
+            if(rgb_matrix_get_flags() == LED_FLAG_UNDERGLOW) {
+
+                for (int i = 1; i < 14; i++) rgb_matrix_set_color(i, 75, 255, 75); //Num row + (-) + (=) + BCKS
+                for (int i = 15; i < 29; i++) rgb_matrix_set_color(i, 75, 255, 75); //QWERT row
+                for (int i = 30; i < 43; i++) rgb_matrix_set_color(i, 75, 255, 75);
+                for (int i = 44; i < 57; i++) rgb_matrix_set_color(i, 75, 255, 75);
+                for (int i = 58; i < 61; i++) rgb_matrix_set_color(i, 75, 255, 75);
+                rgb_matrix_set_color(60, 255, 0, 0); //LGUI
+                rgb_matrix_set_color(62, 255, 0, 0); //RGUI
+                rgb_matrix_set_color(0, 255, 0, 0); //ESC
+                for (int i = 63; i < 67; i++) rgb_matrix_set_color(i, 75, 255, 75);
+                //MACRO KEYS
+                rgb_matrix_set_color(14, 255, 0, 0);
+                rgb_matrix_set_color(29, 255, 0, 0);
+                rgb_matrix_set_color(43, 255, 0, 0);
+                rgb_matrix_set_color(57, 255, 0, 0);
+            }
             break;
         case 1:
-            rgb_matrix_set_color_all(10,10,10);
+            for (int i = 0; i < 67; i++) rgb_matrix_set_color(i, 75, 255, 75);
 
             rgb_matrix_set_color(63, 255, 0, 0);
             for (int i = 0; i < 13; i++) {  // Function row
-                rgb_matrix_set_color(i, 204, 255, 204);
+                rgb_matrix_set_color(i, 255, 128, 0);
             }
             for (int i = 16; i < 21; i++){
-            rgb_matrix_set_color(i, 204, 255, 204);
+            rgb_matrix_set_color(i, 255, 128, 0);
             }
-            for (int i = 31; i < 36; i++){
-            rgb_matrix_set_color(i, 204, 255, 204);
+            for (int i = 31; i < 40; i++){
+            rgb_matrix_set_color(i, 255, 128, 0);
             }
-            rgb_matrix_set_color(45, 204, 255, 204);
+            rgb_matrix_set_color(45, 255, 128, 0);
             for (int i = 49; i < 52; i++){
-            rgb_matrix_set_color(i, 204, 255, 204);
+            rgb_matrix_set_color(i, 255, 128, 0);
             }
-            rgb_matrix_set_color(35, 204, 255, 204);
-            rgb_matrix_set_color(36, 204, 255, 204);
-            rgb_matrix_set_color(24, 204, 255, 204);
+
+
+            rgb_matrix_set_color(24, 255, 128, 0);
 
             break;
         case 2:
-            rgb_matrix_set_color_all(0,0,0);
+            for (int i = 0; i < 67; i++) rgb_matrix_set_color(i, 75, 255, 75);
             rgb_matrix_set_color(30, 255, 0, 0);
-            rgb_matrix_set_color(17, 75, 255, 75);
-            rgb_matrix_set_color(31, 75, 255, 75);
-            rgb_matrix_set_color(32, 75, 255, 75);
-            rgb_matrix_set_color(33, 75, 255, 75);
-            rgb_matrix_set_color(44, 75, 255, 75);
-            rgb_matrix_set_color(45, 75, 255, 75);
-            rgb_matrix_set_color(46, 75, 255, 75);
-            rgb_matrix_set_color(58, 75, 255, 75);
-            rgb_matrix_set_color(59, 75, 255, 75);
-            rgb_matrix_set_color(60, 75, 255, 75);
+            rgb_matrix_set_color(17, 255, 128, 0);
+            for (int i = 31; i < 34; i++) rgb_matrix_set_color(i, 255, 128, 0);
+            for (int i = 44; i < 47; i++) rgb_matrix_set_color(i, 255, 128, 0);
+            for (int i = 58; i < 61; i++) rgb_matrix_set_color(i, 255, 128, 0);
+
             break;
         default:
                 break;
